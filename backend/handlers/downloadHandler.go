5801a9a9c87b802c97b60bb7da32dd1961025630
Olayiwola Salawu <salawuolayiwola181@gmail.com>
2026-04-19 13:23:55 +0100

    feat: compeleted ascii art live preview and download as txt

--- backend/handlers/downloadHandler.go
+++ backend/handlers/downloadHandler.go
@@ -2,12 +2,10 @@ package handlers
 
 import (
 	"errors"
-	"log"
 	"net/http"
 )
 
 func (h *Handler) DownloadHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("DownloadHandler called")
 	Result, err := h.generateArt(r)
 	if errors.Is(err, INVALID_CHAR) {
 		h.clientError(w, http.StatusBadRequest)
@@ -20,6 +18,5 @@ func (h *Handler) DownloadHandler(w http.ResponseWriter, r *http.Request) {
 	fileName := "ascii-art.txt"
 	w.Header().Set("Content-Disposition", "attachment; filename="+fileName)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
 	w.Write([]byte(Result))
 }
